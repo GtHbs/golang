1054232e8443afebd9f3b878258356d476ba44e4
Kerwinnli <Kerwinnli@tencent.com>
2024-05-14 15:55:53 +0800

    1

--- test/calculator.go
+++ test/calculator.go
@@ -1,9 +1,58 @@
 package test
 
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+)
+
 func Add(a, b int) int {
 	return a + b
 }
 
 func Subtract(a, b int) int {
 	return a - b
+}
+
+func Routes() {
+	http.HandleFunc("/sendJson", SendJson)
+}
+
+func SendJson(rw http.ResponseWriter, req *http.Request) {
+	u := struct {
+		Name string
+	}{
+		Name: "John Doe",
+	}
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+	json.NewEncoder(rw).Encode(u)
+}
+
+func MockServer() *httptest.Server {
+	sendJson := func(w http.ResponseWriter, r *http.Request) {
+		u := struct {
+			Name string
+		}{
+			Name: "John Doe",
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(u)
+	}
+	// HandlerFunc接口实现为匿名函数
+	return httptest.NewServer(http.HandlerFunc(sendJson))
+}
+
+func Tag(tag string) string {
+	switch tag {
+	case "1":
+		return "A"
+	case "2":
+		return "B"
+	case "3":
+		return "C"
+	default:
+		return "D"
+	}
 }
--- test/calculator_test.go
+++ test/calculator_test.go
@@ -1,6 +1,11 @@
 package test
 
-import "testing"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 
@@ -11,4 +16,62 @@ func TestAdd(t *testing.T) {
 	if Subtract(2, 3) == -1 {
 		t.Log("2-2=-1")
 	}
+
+	/**
+	表组测试
+	*/
+	sum := Add(2, 2)
+	if sum == 4 {
+		t.Log("2+2=4")
+	} else {
+		t.Fatal("result is wrong")
+	}
+
+	sum = Subtract(2, 2)
+	if sum == 0 {
+		t.Log("2-2=0")
+	} else {
+		t.Fatal("result is wrong")
+	}
+}
+
+/**
+模拟服务端响应
+*/
+//func TestSendJson(t *testing.T) {
+//	Routes()
+//	req, err := http.NewRequest(http.MethodGet, "/sendJson", nil)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//	// 利用http.ResponseWriter模拟服务端响应
+//	rw := httptest.NewRecorder()
+//	http.DefaultServeMux.ServeHTTP(rw, req)
+//	log.Println("code:", rw.Code)
+//	log.Println("body:", rw.Body.String())
+//}
+
+func TestSendJson(t *testing.T) {
+	server := MockServer()
+	defer server.Close()
+	resp, err := http.Get("http://" + server.Listener.Addr().String() + "/json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	log.Println("code:", resp.StatusCode)
+	json, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	log.Println(string(json))
+}
+
+// 使用命令，go test -v -coverprofile=c.out ，指定输出文件
+// 使用命令，go tool cover -html=c.out -o=tag.html，来输出哪些用例未测试
+func TestTag(t *testing.T) {
+	Tag("1")
+	Tag("2")
+	Tag("3")
+	Tag("4")
 }
